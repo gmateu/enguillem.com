4788ea027537022e3696918f9340b07e0f00c9a5
gmateu <gmateu@esliceu.com>
2023-01-06 10:06:31 +0100

    pagina 404

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(  );?>
+
+<main class="container">
+    <div class="pagina404">
+        <h1>Página no encontrada</h1>
+        <h2>
+            <a href="<?php echo home_url( ) ?>">Volver al inicio</a>
+        </h2>
+    </div>
+</main>
+
+
+<?php get_footer(  );?>
