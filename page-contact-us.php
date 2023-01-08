6f6e4816927e3b2c3bf480430521fea3317f3245
gmateu <gmateu@esliceu.com>
2023-01-08 10:05:08 +0100

    contact us formulari

--- page-contact-us.php
+++ page-contact-us.php
@@ -1,7 +1,7 @@
 <?php get_header(  );?>
 
 <main class="container">
-    <div class="lista-tutoriales">
+    <div class="lista-tutoriales my-3">
         <div class="row">
         <?php 
         
@@ -9,25 +9,19 @@
             while ( have_posts( ) ){
                 the_post(  );
         ?>
-            <div class="col">
-                <figure>
-                    <?php the_post_thumbnail('large');?>
-                </figure>
+            <div class="col-12">
                 <h1 class='my-3 text-center'>
-                        <?php the_title();?>
-                    </a>
+                     <?php the_title();?>
                 </h1>
-                
             </div>
             <?php
             } 
         }?>
-        <div class="col">
-
-        <?php
-        echo do_shortcode( '[contact-form-7 id="95" title="Formulario de contacto tutoriales"]' );
-        ?>
-        </div>
+            <div class="col-12">
+                <?php
+                echo do_shortcode( '[contact-form-7 id="95" title="Formulario de contacto tutoriales"]' );
+                ?>
+            </div>
         </div>
     </div>
 </main>
