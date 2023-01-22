30db0c9d558e2a86f43b66df809541b5545eaedd
gmateu <gmateu@esliceu.com>
2023-01-22 11:16:42 +0100

    inclos metabox

--- single-tutorial.php
+++ single-tutorial.php
@@ -1,6 +1,13 @@
 <?php get_header(  );?>
 
 <main class="container my-3">
+<div class="metabox">
+        <p>
+            <a href="<?php echo site_url('tutoriales');?>" class="metabox__home">
+                Tutoriales
+            </a>
+        </p>
+    </div>
     <?php if ( have_posts(  )){
         while ( have_posts( ) ){
             the_post(  );
